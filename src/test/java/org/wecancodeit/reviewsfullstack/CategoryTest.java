3e9b7ecd5784063c26f7a04dac781fb22c38e327
Gilford <vincentgilford@gmail.com>
2018-03-02 10:57:31 -0500

    Category Test added

---
+++ src/test/java/org/wecancodeit/reviewsfullstack/CategoryTest.java
@@ -0,0 +1,20 @@
+package org.wecancodeit.reviewsfullstack;
+
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class CategoryTest {
+	
+	@Test
+	public void shouldGetCategoryForAnimeReview() {
+		Category underTest = new Category("Fantasy");
+		
+		String check = underTest.getCategory();
+		
+		assertThat(check, is("Fantasy"));
+		
+		
+	}
+	
+}
