7db1fb69e896abe0597e84c08ed4b65c3400ff65
Gilford <vincentgilford@gmail.com>
2018-03-02 11:38:20 -0500

    AnimeReview Test

--- src/test/java/org/wecancodeit/reviewsfullstack/AnimeReviewTest.java
+++ src/test/java/org/wecancodeit/reviewsfullstack/AnimeReviewTest.java
@@ -1,5 +1,22 @@
 package org.wecancodeit.reviewsfullstack;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+ 
 public class AnimeReviewTest {
-
+	
+	
+	@Test
+	public void shouldRetrieveAnimeTitle() {
+		AnimeReview underTest = new AnimeReview("Naruto");
+		
+		String check = underTest.getAnimeReview(); 
+		
+		assertThat(check, is("Naruto"));
+		
+		
+		
+		
+	}
 }
