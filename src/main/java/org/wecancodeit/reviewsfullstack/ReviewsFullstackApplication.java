102869afb2f25f0ab08527181c830ece2c5cb6d0
Gilford <vincentgilford@gmail.com>
2018-03-02 10:04:32 -0500

    Initial commit

---
+++ src/main/java/org/wecancodeit/reviewsfullstack/ReviewsFullstackApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.reviewsfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewsFullstackApplication.class, args);
+	}
+}
