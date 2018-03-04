d085d91c3ffa7476a2f2cfc8cab46893ad70e547
Gilford <vincentgilford@gmail.com>
2018-03-04 11:52:45 -0500

    All html files working, now submitting images

--- src/main/java/org/wecancodeit/reviewsfullstack/CategoryRepository.java
+++ src/main/java/org/wecancodeit/reviewsfullstack/CategoryRepository.java
@@ -12,6 +12,6 @@ import org.springframework.data.repository.CrudRepository;
  *
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
-	List<Category> findByCategoryTitle(String category);
+	List<Category> findByCategoryTitle(String categoryTitle);
 	
 }
